9c5be164031b30c4d4b6b309f6e4c27e5195f386
Shm3kvud <paraskevopuloskonstantinos37@gmail.com>
2026-01-03 00:41:29 +0300

    finally validators.py

--- app/middlewares.py
+++ app/middlewares.py
@@ -1,5 +1,20 @@
 from aiogram import BaseMiddleware
+from aiogram.types import Message
+from typing import Callable, Dict, Any, Awaitable
 
 
-class HaveText(BaseMiddleware):
-    pass
+class TextCheckMiddleware(BaseMiddleware):
+    async def __call__(self,
+                       handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
+                       event: Message,
+                       data: Dict[str, Any]) -> Any:
+        state = data.get('state')
+        
+        if state:
+            current_state = await state.get_state()
+            if current_state: 
+                if event.text is None:
+                    await event.answer(text="Кажется, ты не ввел текст, попробуй еще раз.")
+                    return
+        
+        return await handler(event, data)
